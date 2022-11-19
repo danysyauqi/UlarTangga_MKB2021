2503ca5addd7996c4233431a21043d459f6296fd
danysyauqi <93965029+danysyauqi@users.noreply.github.com>
2022-11-19 15:54:07 +0700

    ulartangga_demo-ver-0.1
    Application with javafx base from Java Programming

---
+++ demo_ulartanggamkb/src/main/java/com/app/PrimaryController.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+    @FXML
+    private void switchToHistory() throws IOException{
+        App.setRoot("history");
+    }
+}
